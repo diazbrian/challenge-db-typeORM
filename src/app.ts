ae300c40a5d0c0b5933b00d22410c0b52539edc2
Brian Diaz <diazbrian1666@gmail.com>
2024-05-31 16:04:18 -0300

    fix(app): add express.json

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json()); // para analizar el body de las peticiones POST (req.body)
 
 // `/api` ruta de acceso general que nos lleva a `usersRouter`  (alias de users.router.ts)
 // app.use("/api", usersRouter);
