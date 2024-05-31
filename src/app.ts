9f94c60119df65d1d2b9071f96f9a696f7ab8e01
Brian Diaz <diazbrian1666@gmail.com>
2024-05-31 14:01:22 -0300

    feat(api): add eventRouter

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import usersRouter from './routes/users.router';
+import eventRouter from './routes/event.router';
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
 
 // `/api` ruta de acceso general que nos lleva a `usersRouter`  (alias de users.router.ts)
-app.use("/api", usersRouter);
+// app.use("/api", usersRouter);
+app.use("/api", eventRouter);
 
 export default app;
