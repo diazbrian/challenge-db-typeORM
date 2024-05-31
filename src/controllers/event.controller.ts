853b8f083f31d008545f64b7b9334066f0131a6f
Brian Diaz <diazbrian1666@gmail.com>
2024-05-31 14:50:35 -0300

    feat: add Event methods

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Event as User } from '../entities/Event';
 
 // export para ser usado en users.router
-export const getUsers = async (req: Request, res: Response) => {
+export const getAllUsers = async (req: Request, res: Response) => {
     console.log('entrando...');
     try {
         const users = await User.find(); // `find` sin parametros devuelve todos los registros
@@ -14,3 +14,64 @@ export const getUsers = async (req: Request, res: Response) => {
         }
     }
 };
+
+export const getUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params; // `id` viene de la url
+        const user = await User.findOneBy({ id: parseInt(id) });
+
+        if (!user)
+            return res.status(404).json({ message: 'User not found' });
+
+        return res.json(user);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+};
+
+
+export const createUser = async (req: Request, res: Response) => {
+    const { type, firstname, gps } = req.body;
+    const user = new User();
+    user.type = type;
+    user.firstname = firstname;
+    user.gps = gps;
+    await user.save();
+    return res.json(user);
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findOneBy({ id: parseInt(id) });
+        if (!user)
+            return res.status(404).json({ message: 'User not found' });
+
+        await User.update({ id: parseInt(id) }, req.body);
+
+        return res.sendStatus(204);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await User.delete({ id: parseInt(id) });
+
+        if (result.affected === 0)
+            return res.status(404).json({ message: 'User not found' });
+
+        return res.sendStatus(204);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+
+}
